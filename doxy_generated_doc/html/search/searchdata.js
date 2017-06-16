928903bbc5f4dd8678f5462964cb123241acae8a
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2017-06-16 17:02:24 -0500

    c2

---
+++ doxy_generated_doc/html/search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "abcdilmnopqrstuw",
+  1: "bcdilnpqs",
+  2: "bms",
+  3: "abcdimpstw",
+  4: "biost",
+  5: "b",
+  6: "b",
+  7: "b",
+  8: "brt",
+  9: "dmsu"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "groups"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Modules"
+};
+
